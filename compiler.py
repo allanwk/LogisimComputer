f4a0e84f3cc8262a5c1e0d8f83f7c7fba4764d20
allan <allankretzmann@hotmail.com>
2021-09-04 10:16:01 -0300

    Initial commit

---
+++ compiler.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import numpy as np
+
+#Get instruction codes from spreadsheet
+df = pd.read_excel('16bit_set.xlsx', dtype=str)
+df = df.set_index(df['Instruction'])
+instructions = pd.unique(df['Instruction'])
+
+codes = {}
+for instruction in instructions[1:]:
+    codes[instruction] = df.loc[instruction, ['Code']].iloc[0]['Code']
+
+def compile(filename):
+    """
+    Compiles a simple language containing labels, jumps (gotos) and constants,
+    generating a text file with hex values for the computer RAM.
+    """
+
+    with open(filename, 'r') as file:
+        lines = file.read().splitlines()
+    var_address = 192
+    constants = {}
+    memory = {}
+    labels = {}
+    instruction_address_counter = 0
+
+    #Extract constants and commands
+    const = [line.split('//')[0].strip() for line in lines if line != '' and '=' in line]
+    commands = [line.split('//')[0].strip() for line in lines if line != '' and '=' not in line]
+    
+    #Map constants to memory
+    for c in const:
+        name, value = c.replace(" ", "").split("=")
+        constants[name] = var_address
+        memory[var_address] = hex(int(value))[2:].zfill(4)
+        var_address += 1
+
+    #Map labels to memory
+    for index, line in enumerate(commands):
+        if ':' in line:
+            label = line[:line.index(':')]
+            labels[label] = var_address
+            memory[var_address] = hex(int(index))[2:].zfill(4)
+            var_address += 1
+            commands[index] = commands[index].split(':')[1].strip()
+    
+    """For each line, create a binary string containing 8 instruction bits
+    and 8 address bits. Convert this string to hex and save to memory dict.
+    The mrmory dict is used to generate a numpy array, which is saved as
+    the RAM.txt file.
+    """
+    for line in commands:
+        l = line.split(" ")
+        hex_str = ""
+
+        if len(l) == 2:
+            instruction, operand = l
+            binary_str = codes[instruction]
+            if operand in labels:
+                binary_str += bin(int(labels[operand]))[2:].zfill(8)
+            elif operand in constants:
+                binary_str += bin(int(constants[operand]))[2:].zfill(8)
+            else:
+                memory[var_address] = hex(int(operand))[2:].zfill(4)
+                constants[operand] = var_address
+                var_address += 1
+                binary_str += bin(int(constants[operand]))[2:].zfill(8)
+            
+            hex_str = hex(int(binary_str, 2))[2:].zfill(4)
+                
+
+        else:
+            instruction = l[0]
+            binary_str = codes[instruction] + "0"*8
+            hex_str = hex(int(binary_str, 2))[2:].zfill(4)
+        
+        memory[instruction_address_counter] = hex_str
+        instruction_address_counter += 1
+
+    arr = np.full((1,256), '0', dtype=np.dtype('U100'))
+    for key, value in memory.items():
+        arr[0,key] = value
+    arr = arr.reshape(16,16)
+
+    np.savetxt('RAM.txt', arr, delimiter=' ', fmt='%s')
+
+    with open('RAM.txt', 'r+') as f:
+        line = 'v2.0 raw'
+        content = f.read()
+        f.seek(0, 0)
+        f.write(line.rstrip('\r\n') + '\n' + content)
+
+    print("Compiled to RAM.txt")   
+
+if __name__ == '__main__':
+    compile('TuringMachine.txt')
---
+++ create_rom_file.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+#Opening spreadsheet containing instruction codes and control words
+df = pd.read_excel('16bit_instruction_set.xlsx', dtype=str)
+n_instructions = len(df)
+
+#Creating multiples for handling flag signals
+df = pd.concat([df]*32, ignore_index=True)
+
+#List of all possibilities for the 5 flag bits
+flags_list = [bin(i)[2:].zfill(5) for i in range(pow(2, 5))]
+flags_df = pd.DataFrame(np.repeat(flags_list, n_instructions, axis=0), columns=['Flags'])
+
+#Check if flag at given position is set
+def check_flag(string, index):
+    return string[-(index + 1)] == '1'
+
+#Combining instruction dataframe with flags and organizing columns
+df = pd.concat([df, flags_df], axis=1)
+cols = df.columns.tolist()
+cols = cols[:3] + cols[-1:] + cols[3:-1]
+df = df[cols]
+
+#Getting ROM addresses in decimal to create Logisim memory file
+data = {}
+converted_addr = []
+for index, row in df[['Code', 'Step', 'Flags']].iterrows():
+    dec_addr = int(str(row['Code']) + str(row['Step']) + str(row['Flags']), base=2)
+    data[dec_addr] = 0
+    converted_addr.append(dec_addr)
+
+df['Code'] = converted_addr
+
+#Setting microcode for conditional jumps
+for i in tqdm(range(len(df))):
+    if df.loc[i, ['Instruction']]['Instruction'] == 'JEQ' and check_flag(df.loc[i, ['Flags']]['Flags'], 2) and df.loc[i, ['Step']]['Step'] == '011':
+        df.loc[i, ['RO']] = 1
+        df.loc[i, ['J']] = 1
+    elif df.loc[i, ['Instruction']]['Instruction'] == 'JG' and check_flag(df.loc[i, ['Flags']]['Flags'], 3) and df.loc[i, ['Step']]['Step'] == '011':
+        df.loc[i, ['RO']] = 1
+        df.loc[i, ['J']] = 1
+    elif df.loc[i, ['Instruction']]['Instruction'] == 'JL' and check_flag(df.loc[i, ['Flags']]['Flags'], 4) and df.loc[i, ['Step']]['Step'] == '011':
+        df.loc[i, ['RO']] = 1
+        df.loc[i, ['J']] = 1
+
+#Generating microcode from columns and saving to data dict
+for index, row in df.iterrows():
+    binary_str = ""
+    for col in df.columns[4:]:
+        binary_str += str(row[col])
+
+    data[row['Code']] = hex(int(binary_str, base=2))
+
+#Converting data to array and save to text file
+arr = np.full((1,pow(2, 16)), '0', dtype=np.dtype('U100'))
+for key, value in data.items():
+    arr[0,key] = value.split('x')[1]
+arr = arr.reshape(4096,16)
+np.savetxt('instructions_rom16.txt', arr, delimiter=' ', fmt='%s')
+
+#Adding Logisim memory header
+with open('instructions_rom16.txt', 'r+') as f:
+    line = 'v2.0 raw'
+    content = f.read()
+    f.seek(0, 0)
+    f.write(line.rstrip('\r\n') + '\n' + content)
